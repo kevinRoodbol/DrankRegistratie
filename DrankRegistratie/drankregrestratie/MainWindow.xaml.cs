cc20459c91eb1f1aff5fe5b5e75e724e7d33aced
kevinRoodbol <60099615+kevinRoodbol@users.noreply.github.com>
2020-04-13 23:53:09 +0200

    Upload files to repo
    Update

---
+++ DrankRegistratie/drankregrestratie/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace drankregrestratie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        drankregrestratie db = new drankregrestratie();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dgdrank.ItemsSource = db.items.ToList();
+        }
+
+        private void btnOpslaan_Click(object sender, RoutedEventArgs e)
+        {
+            Naam deNaam = new Naam();
+            deNaam.Naam = txtboxnaam.Text
+
+            db.items.InsertOnSubmit(deNaam);
+
+            db.SubmitChanges();
+
+            dgItems.ItemsSource = db.items.ToList();
+        }
+
+        private void btnzoek_Click(object sender, RoutedEventArgs e)
+        {
+            string sWaarde = txtzoek.Text;
+
+            var lijst = db.items.Where(p => p.itemnaam.Contains(sWaarde)).ToList();
+            dgItems.ItemsSource = lijst;
+        }
+    }
+}
